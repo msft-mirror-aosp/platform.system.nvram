60e6bd85da97190f63868c5ec339f2189f1db9ab
Mattias Nissler <mnissler@google.com>
2016-03-21 17:47:04 +0100

    Rewire testing NVRAM HAL module to talk to fake-nvram
    After this change, the nvram.testing HAL module doesn't host the
    access-controlled NVRAM HAL reference implementation in memory, but
    passes calls to the fake-nvram daemon via socket IPC instead.
    
    BUG: 27764639
    Change-Id: Ic8ccf66a59878b3f20ac8ea6a1c17702d4910b5a

--- hal/Android.mk
+++ hal/Android.mk
@@ -29,18 +29,18 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_SHARED_LIBRARIES := libnvram-messages
 include $(BUILD_STATIC_LIBRARY)
 
-# nvram.testing is the software-only testing NVRAM HAL module.
+# nvram.testing is the software-only testing NVRAM HAL module backed by the
+# fake_nvram daemon.
 include $(CLEAR_VARS)
 LOCAL_MODULE := nvram.testing
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
-	memory_storage.cpp \
 	testing_module.c \
 	testing_nvram_implementation.cpp
 LOCAL_CLANG := true
 LOCAL_CFLAGS := -Wall -Werror -Wextra -fvisibility=hidden
-LOCAL_STATIC_LIBRARIES := libnvram-core libnvram-hal libmincrypt liblog
-LOCAL_SHARED_LIBRARIES := libnvram-messages
+LOCAL_STATIC_LIBRARIES := libnvram-hal
+LOCAL_SHARED_LIBRARIES := libnvram-messages libcutils libbase
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
--- hal/testing_nvram_implementation.cpp
+++ hal/testing_nvram_implementation.cpp
@@ -16,14 +16,19 @@
 
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include <hardware/nvram.h>
+#include <android-base/logging.h>
+#include <cutils/sockets.h>
 
-#include <nvram/core/nvram_manager.h>
 #include <nvram/hal/nvram_device_adapter.h>
 #include <nvram/messages/nvram_messages.h>
 
 namespace {
+
+constexpr char kFakeNvramControlSocketName[] = "nvram";
 
 // This instantiates an |NvramManager| with the storage interface wired up with
 // an in-memory implementation. This *DOES NOT* meet the persistence and tamper
@@ -31,44 +36,94 @@ namespace {
 // tests against the |NvramManager| implementation.
 class TestingNvramImplementation : public nvram::NvramImplementation {
  public:
-  ~TestingNvramImplementation() override = default;
+  ~TestingNvramImplementation() override;
 
   void Execute(const nvram::Request& request,
-               nvram::Response* response) override {
-    nvram::Blob request_blob;
-    if (!nvram::Encode(request, &request_blob)) {
-      response->result = NV_RESULT_INVALID_PARAMETER;
-      return;
-    }
-    nvram::Blob response_blob;
-    if (!ExecuteEncoded(request_blob, &response_blob)) {
-      response->result = NV_RESULT_INTERNAL_ERROR;
-      return;
-    }
-    if (!nvram::Decode(response_blob.data(), response_blob.size(), response)) {
-      response->result = NV_RESULT_INTERNAL_ERROR;
-      return;
-    }
-  }
+               nvram::Response* response) override;
 
  private:
-  // Mixing encoding into the Execute flow emulates scenarios where requests are
-  // sent to another component for processing.
-  bool ExecuteEncoded(const nvram::Blob& request_blob,
-                      nvram::Blob* response_blob) {
-    nvram::Request request;
-    if (!nvram::Decode(request_blob.data(), request_blob.size(), &request)) {
-      return false;
+  // Connects the fake NVRAM control socket it it is not open already. Returns
+  // true if the channel is open, false on errors.
+  bool Connect();
+
+  // Sends a request to the fake NVRAM daemon. Returns true if successful, false
+  // on any I/O errors.
+  bool SendRequest(const nvram::Request& request, nvram::Response* response);
+
+  // A file descriptor of the socket connected to the fake NVRAM daemon.
+  int nvram_socket_fd_ = -1;
+
+  // The command buffer, used for encoding request and decoding responses.
+  uint8_t command_buffer_[4096];
+};
+
+TestingNvramImplementation::~TestingNvramImplementation() {
+  if (nvram_socket_fd_ != -1) {
+    // No need to handle EINTR specially here as bionic filters it out.
+    if (close(nvram_socket_fd_)) {
+      PLOG(ERROR) << "Failed to close NVRAM command socket";
     }
-    nvram::Response response;
-    nvram_manager_.Dispatch(request, &response);
-    if (!nvram::Encode(response, response_blob)) {
-      return false;
-    }
+    nvram_socket_fd_ = -1;
+  }
+}
+
+void TestingNvramImplementation::Execute(const nvram::Request& request,
+                                         nvram::Response* response) {
+  if (!SendRequest(request, response)) {
+    response->result = NV_RESULT_INTERNAL_ERROR;
+  }
+}
+
+bool TestingNvramImplementation::Connect() {
+  if (nvram_socket_fd_ != -1) {
     return true;
   }
-  nvram::NvramManager nvram_manager_;
-};
+
+  int rc =
+      socket_local_client(kFakeNvramControlSocketName,
+                          ANDROID_SOCKET_NAMESPACE_RESERVED, SOCK_SEQPACKET);
+  if (rc < 0) {
+    PLOG(ERROR) << "Failed to connect fake NVRAM control socket";
+    return false;
+  }
+
+  nvram_socket_fd_ = rc;
+  return true;
+}
+
+bool TestingNvramImplementation::SendRequest(const nvram::Request& request,
+                                             nvram::Response* response) {
+  if (!Connect()) {
+    return false;
+  }
+
+  size_t request_size = sizeof(command_buffer_);
+  if (!nvram::Encode(request, command_buffer_, &request_size)) {
+    LOG(ERROR) << "Failed to encode NVRAM request.";
+    return false;
+  }
+
+  ssize_t rc = TEMP_FAILURE_RETRY(
+      write(nvram_socket_fd_, command_buffer_, request_size));
+  if (rc < 0) {
+    PLOG(ERROR) << "Failed to send request on NVRAM control socket";
+    return false;
+  }
+
+  rc = TEMP_FAILURE_RETRY(
+      read(nvram_socket_fd_, command_buffer_, sizeof(command_buffer_)));
+  if (rc < 0 || static_cast<size_t>(rc) > sizeof(command_buffer_)) {
+    PLOG(ERROR) << "Failed to read NVRAM response";
+    return false;
+  }
+
+  if (!nvram::Decode(command_buffer_, static_cast<size_t>(rc), response)) {
+    LOG(ERROR) << "Failed to decode NVRAM response.";
+    return false;
+  }
+
+  return true;
+}
 
 }  // namespace
 
